92d8efdde8441dd70cac52bd26e745902b0f10ae
oshan2csd <oshan2csd@gmail.com>
2018-02-21 19:01:18 -0800

    Added Data Types

--- ReadMe.md
+++ ReadMe.md
@@ -6,4 +6,10 @@
 2. var, let and const
     * var and let behave same in most cases (apart from scope)
     * const - for a const object/arrays it can not reassign, but values inside can be reassigned
-    
+
+3. Data types
+    * primimtve (value type) - access from the variable 
+        strings/numbers(all int,decimal, floats)/boolean/null/undefined(if a variable   is unassigned)/ Symbols
+    * reference - access from memory location (heap)
+        Arrays/object literals/functions/dates/anytthing else...
+    * JS is dynamically typed (no need to specify types)
--- app.js
+++ app.js
@@ -38,45 +38,74 @@ console.timeEnd('hello');
 //==========================================
 //var, let, const
 //var & let works same in most cases, except scope
-var name = 'Oshan Silva';// strings can be double or single quotes
-console.log(name);
+//var name = 'Oshan Silva';// strings can be double or single quotes
+//console.log(name);
 
 //variable conventions
 //can not start with numbers
 //letters, $, numbers are ok
 
 //CONST
-const student = 'oshan';
-console.log(student);
+//const student = 'oshan';
+//console.log(student);
 //can not reassigne
 //student = 'kasuni';
 
 //can not initialize without a value (declaration required)
 //const car;
 
-//objects
-const person = {
-  name: 'kasuni',
-  age:30
-}
+// //objects
+// const person = {
+//   name: 'kasuni',
+//   age:30
+// }
 
-console.log(person);
+// console.log(person);
 
-//for const objects, object itself can not re assign
-//but, data inside object can changed
-//EVEN IF OBJECT IS "CONST"
+// //for const objects, object itself can not re assign
+// //but, data inside object can changed
+// //EVEN IF OBJECT IS "CONST"
 
-person.name = 'oshan';
-person.age = 35;
-console.log(person);
+// person.name = 'oshan';
+// person.age = 35;
+// console.log(person);
 
-//same thing applied for arrays as well
-//const doesn't mean it has to remain unchanged
-//it just can not re assign
-const numbers = [1,2,3,4,4];
-numbers.push(8);
-numbers.pop();
-console.log(numbers);
+// //same thing applied for arrays as well
+// //const doesn't mean it has to remain unchanged
+// //it just can not re assign
+// const numbers = [1,2,3,4,4];
+// numbers.push(8);
+// numbers.pop();
+// console.log(numbers);
 
 
 //==========================================
+//Data types
+
+  //PRIMITIVE
+      //String
+      const name = 'oshan';
+      //number
+      const age = 45;
+      //boolean
+      const haskids = true;
+      //Null
+      const car = null;
+      //undefined
+      var student;
+      //symbol
+      const symb = Symbol();
+
+  //REFERENCE TYPES - Objects
+      //Array
+      const hobbies = ['movies', 'music'];
+      //Object
+      const address = {
+        city:'Mel',
+        state:'VIC'
+      }
+      //Date
+      const today = new Date();
+      
+console.log(today);
+console.log(typeof today);
